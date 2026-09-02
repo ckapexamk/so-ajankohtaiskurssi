096e2b50bf72bbd330ba6aae214067f4a67e1b78
Kalle Perttunen <ckape008@edu.xamk.fi>
2026-09-02 19:27:57 +0300

    S1-11: cors middleware in main.py, client.ts handles apiurls, which is used to show api health on HomePage

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,26 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 
 from app.api.health import router as health_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 # config testaus
-@app.get("/config")
+@app.get("/")
 def config_check():
     return {
         "api_port": settings.api_port,
-        "database_configured": bool(settings.database_url),
+        "database_url": bool(settings.database_url),
     }
 
 
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react';
+import apiReq from '../api/client';
+
+interface HealthResponse {
+    status: string;
+}
+
 const HomePage : React.FC = () : React.ReactElement => {
-  return (
-    <>
-      <h2>Sprint 1 placeholder</h2>  
-    </>
-  );
+    const [health, setHealth] = useState<string>("Tarkistetaan palvelinta...");
+
+    useEffect(() => {
+        apiReq<HealthResponse>("/health")
+            .then((response) => {
+                setHealth(response.status === "ok"
+                    ? "OK"
+                    : "failed"
+                );
+            })
+            .catch((error) => {
+                console.error(error)
+                setHealth("failed");
+            });
+    }, []);
+
+    return (
+        <>
+            <h2>Sprint 1 placeholder</h2>
+            <h4>API health:</h4><p>{health}</p>
+        </>
+    );
 }
 
 export default HomePage
