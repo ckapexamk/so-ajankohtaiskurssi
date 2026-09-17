978d62e258d07248750b0c915be4eaf4bb6db776
Kalle Perttunen <ckape008@edu.xamk.fi>
2026-09-17 13:39:58 +0300

    S2-02: user model added

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,13 @@
+from app.models.user import User
+
+__all__ = ["User"]
+
+
+#   alembic/env.py
+#from app.db.base import Base
+#from app.models.user import User
+
+#target_metadata = Base.metadata
+
+
+
